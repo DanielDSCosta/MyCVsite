987186eb1f87b7a387cd1c0adc81d11be916deca
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2024-08-06 16:05:19 +0200

    Ajout Header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import './App.css';
 import { Header, Footer } from './components';
 import { Home, About, Skills, Courses, Achievements, Contact } from './pages';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header>
-      <h1 className="header">My React App</h1>
-    </header>
+    <>
+      <header className="header">
+        <nav className="header__nav">
+          <NavLink to="/" className="header__link" activeClassName="header__link--active">
+            <i className="fa-solid fa-house"></i>
+          </NavLink>
+          <NavLink to="/page2" className="header__link" activeClassName="header__link--active">Page 2</NavLink>
+          <NavLink to="/page3" className="header__link" activeClassName="header__link--active">Page 3</NavLink>
+          <NavLink to="/page4" className="header__link" activeClassName="header__link--active">Page 4</NavLink>
+          <NavLink to="/page5" className="header__link" activeClassName="header__link--active">Page 5</NavLink>
+          <NavLink to="/page6" className="header__link" activeClassName="header__link--active">Page 6</NavLink>
+        </nav>
+      </header>
+      <button className="header__menu-button" onClick={toggleMenu}>
+        ☰
+      </button>
+      <div className={`header__popup ${menuOpen ? 'header__popup--open' : ''}`}>
+        <NavLink to="/" className="header__popup__link" activeClassName="header__link--active" onClick={toggleMenu}>
+          <i className="fa-solid fa-house"></i>
+        </NavLink>
+        <div className="header__popup__separator"></div>
+        <NavLink to="/page2" className="header__popup__link" activeClassName="header__link--active" onClick={toggleMenu}>Page 2</NavLink>
+        <div className="header__popup__separator"></div>
+        <NavLink to="/page3" className="header__popup__link" activeClassName="header__link--active" onClick={toggleMenu}>Page 3</NavLink>
+        <div className="header__popup__separator"></div>
+        <NavLink to="/page4" className="header__popup__link" activeClassName="header__link--active" onClick={toggleMenu}>Page 4</NavLink>
+        <div className="header__popup__separator"></div>
+        <NavLink to="/page5" className="header__popup__link" activeClassName="header__link--active" onClick={toggleMenu}>Page 5</NavLink>
+        <div className="header__popup__separator"></div>
+        <NavLink to="/page6" className="header__popup__link" activeClassName="header__link--active" onClick={toggleMenu}>Page 6</NavLink>
+      </div>
+    </>
   );
 }
 
