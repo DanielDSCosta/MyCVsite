a5a85a31074e0e798ea171176f0721b1f715df2a
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2024-08-11 15:52:30 +0200

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,12 @@ import './Footer.scss';
 
 function Footer() {
   return (
-    <footer>
-      <p>&copy; 2023 My React App</p>
+    <footer className="footer">
+      <a href="https://www.linkedin.com/in/danieldsc/" target="_blank" rel="noopener noreferrer">
+        <i className="fab fa-linkedin"></i>
+      </a>
+      <p>Daniel DA SILVA COSTA © 2024</p>
+      <p>Tous droits réservés</p>
     </footer>
   );
 }
