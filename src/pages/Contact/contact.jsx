1bbbe407c21c51446bb151e3d4fcc89a713140b0
DanielDSCosta <131755890+DanielDSCosta@users.noreply.github.com>
2024-08-11 15:47:32 +0200

    Page Contact

--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -3,9 +3,29 @@ import './Contact.scss'; // Import CSS for Contact page
 
 function Contact() {
   return (
-    <div>
-      <h2>Contact Page</h2>
-      <p>This is the Contact page.</p>
+    <div className="contact-container">
+      <h2 className="contact-title">Contactez-moi !</h2>
+      <p className="contact-description">
+        Actuellement à la recherche d'un nouvel emploi, je suis intéressée par les postes de :
+      </p>
+      <ul className="contact-list">
+        <li><strong>Développeur front-end</strong></li>
+        <li><strong>Intégrateur web</strong></li>
+      </ul>
+      <div className="contact-cards">
+        <div className="contact-card card-phone">
+          <i className="fas fa-phone"></i>
+          <p>+33 6 09 24 81 45</p>
+        </div>
+        <div className="contact-card card-email">
+          <i className="fas fa-envelope"></i>
+          <p>Daniel.dasilva.gsi@gmail.com</p>
+        </div>
+        <div className="contact-card card-meet">
+          <strong>Vous souhaitez me rencontrer ?</strong>
+          <p><br />Je suis disponible à <strong>Montpellier</strong> et ses alentours</p>
+        </div>
+      </div>
     </div>
   );
 }
